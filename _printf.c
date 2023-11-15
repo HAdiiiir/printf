7414cce52692abd3fd5b186593a3e90bee489e6e
Hadeer Jaamal <133434041+HAdiiiir@users.noreply.github.com>
2023-11-15 08:34:29 -0800

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,22 +1,28 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-int _printf(const char *format, ...) {
+int _printf(const char *format, ...)
+{
     va_list args;
     va_start(args, format);
 
-    int count = 0;  // to keep track of the number of characters printed
+    int count = 0;
 
-    while (*format != '\0') {
-        if (*format == '%') {
-            format++;  // move to the next character after '%'
-            switch (*format) {
+    while (*format != '\0')
+        {
+        if (*format == '%') 
+        {
+            format++;
+            switch (*format)
+                {
                 case 'c':
                     count += putchar(va_arg(args, int));
                     break;
-                case 's': {
+                case 's': 
+                    {
                     const char *str = va_arg(args, const char *);
-                    while (*str != '\0') {
+                    while (*str != '\0')
+                    {
                         count += putchar(*str);
                         str++;
                     }
@@ -26,11 +32,12 @@ int _printf(const char *format, ...) {
                     count += putchar('%');
                     break;
                 default:
-                    // Handle unsupported conversion specifier or other characters
                     count += putchar('%');
                     count += putchar(*format);
             }
-        } else {
+        } 
+        else
+        {
             count += putchar(*format);
         }
         format++;
