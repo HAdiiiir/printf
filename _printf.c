d82f502a33bbb173e99caad8b22cc06b4ec54a27
Hadeer Jaamal <133434041+HAdiiiir@users.noreply.github.com>
2023-11-15 03:09:13 -0800

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,69 +1,46 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-int main(void)
-{
-    int age = 25;
-    myprintf("My name is %s and I am %d years old.\n", "Snehasish", age);
-    double pi = 3.14;
-    myprintf("The value of pi is %.2f.\n", pi);
-    // Print a date and time.
-    myprintf("%s %s\n", "Today is", __DATE__);
-    myprintf("%s %s\n", "The time is", __TIME__);
-
-    // Print a list of numbers.
-    int numbers[] = {1, 2, 3, 4, 5};
-    for (int i = 0; i < sizeof(numbers) / sizeof(numbers[0]); i++)
-    {
-        myprintf("%d\n", numbers[i]);
-    }
-
-    // Print a table of data.
-    myprintf("| Name | Age | Occupation |\n");
-    myprintf("| Snehasish | 25 | AI Engineer |\n");
-    myprintf("| Alice | 30 | Software Engineer |\n");
-}
-int myprintf(const char *format, ...)
-{
+int _printf(const char *format, ...) {
     va_list args;
     va_start(args, format);
 
-    int count = 0;
-    while (*format != '\0')
-    {
-        if (*format != '%')
-        {
-            putchar(*format);
-            count++;
-        }
-        else
-        {
-            // Handle format specifiers
-            switch (*++format)
-            {
-            case 'd':
-                count += fprintf(stdout, "%d", va_arg(args, int));
-                break;
-            case 'c':
-                count += fprintf(stdout, "%c", va_arg(args, char));
-                break;
-            case 's':
-                count += fprintf(stdout, "%s", va_arg(args, char *));
-                break;
-            case 'f':
-                count += fprintf(stdout, "%f", va_arg(args, double));
-                break;
-            default:
-                // Handle unknown format specifiers
-                putchar('%');
-                putchar(*format);
-                count += 2;
-                break;
+    int count = 0;  // to keep track of the number of characters printed
+
+    while (*format != '\0') {
+        if (*format == '%') {
+            format++;  // move to the next character after '%'
+            switch (*format) {
+                case 'c':
+                    count += putchar(va_arg(args, int));
+                    break;
+                case 's': {
+                    const char *str = va_arg(args, const char *);
+                    while (*str != '\0') {
+                        count += putchar(*str);
+                        str++;
+                    }
+                    break;
+                }
+                case '%':
+                    count += putchar('%');
+                    break;
+                default:
+                    // Handle unsupported conversion specifier or other characters
+                    count += putchar('%');
+                    count += putchar(*format);
             }
+        } else {
+            count += putchar(*format);
         }
         format++;
     }
 
     va_end(args);
-    return (count);
+    return count;
+}
+
+int main() {
+    _printf("Hello, %s! The character is %c and the percentage is %%.\n", "World", 'A');
+    return 0;
 }
