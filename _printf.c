7c6fb9e61da7a703a554e1966f2372f528f057d7
Hadeer Jaamal <133434041+HAdiiiir@users.noreply.github.com>
2023-11-15 02:14:57 -0800

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,114 +1,47 @@
-#include "main.h"
-#include <stdlib.h>
-#include <unistd.h>
+#include <stdio.h>
+#include <stdarg.h>
 
-/**
-  * find_function - function that finds formats for _printf
-  * calls the corresponding function.
-  * @format: format (char, string, int, decimal)
-  * Return: NULL or function associated ;
-  */
-int (*find_function(const char *format))(va_list)
-{
-	unsigned int i = 0;
-	code_f find_f[] = {
-		{"c", print_char},
-		{"s", print_string},
-		{"i", print_int},
-		{"d", print_dec},
-		{"r", print_rev},
-		{"b", print_bin},
-		{"u", print_unsig},
-		{"o", print_octal},
-		{"x", print_x},
-		{"X", print_X},
-		{"R", print_rot13},
-		{NULL, NULL}
-	};
-
-	while (find_f[i].sc)
-	{
-		if (find_f[i].sc[0] == (*format))
-			return (find_f[i].f);
-		i++;
-	}
-	return (NULL);
-}
-
-/**
- * itoa - Converts an integer to a string
- * @val: Integer to convert
- * @base: Number base for conversion
- *
- * Return: Pointer to the converted string
- */
-char *itoa(int val, int base) {
-    static char buf[32] = {0};
-    int i = 30;
-
-    for (; val && i; --i, val /= base)
-        buf[i] = "0123456789abcdef"[val % base];
-
-    return &buf[i + 1];
-}
-
-
-/**
- * _printf - Custom printf function to format and print data
- * @format: format string containing the directives
- * 
- * Return: Number of characters printed
- */
-int _printf(const char *format, ...)
+int myprintf(const char *format, ...)
 {
     va_list args;
-    int count = 0;
-    char ch;
-    char *str;
-    int val;
-
     va_start(args, format);
 
-    while (*format) {
-        if (*format == '%') {
-            format++;
-            switch (*format) {
-                case 'c':
-                    ch = (char)va_arg(args, int);
-                    putchar(ch);
-                    count++;
-                    break;
-                case 's':
-                    str = va_arg(args, char *);
-                    while (*str) {
-                        putchar(*str);
-                        str++;
-                        count++;
-                    }
-                    break;
-                case 'd':
-                case 'i':
-                    val = va_arg(args, int);
-                    str = itoa(val, 10);
-                    while (*str) {
-                        putchar(*str);
-                        str++;
-                        count++;
-                    }
-                    break;
-                case '%':
-                    putchar('%');
-                    count++;
-                    break;
-            }
-        } else {
+    int count = 0;
+    while (*format != '\0')
+    {
+        if (*format != '%')
+        {
             putchar(*format);
             count++;
+        }
+        else
+        {
+            // Handle format specifiers
+            switch (*++format)
+            {
+            case 'd':
+                count += fprintf(stdout, "%d", va_arg(args, int));
+                break;
+            case 'c':
+                count += fprintf(stdout, "%c", va_arg(args, char));
+                break;
+            case 's':
+                count += fprintf(stdout, "%s", va_arg(args, char *));
+                break;
+            case 'f':
+                count += fprintf(stdout, "%f", va_arg(args, double));
+                break;
+            default:
+                // Handle unknown format specifiers
+                putchar('%');
+                putchar(*format);
+                count += 2;
+                break;
+            }
         }
         format++;
     }
 
     va_end(args);
-
-    return count;
+    return (count);
 }
