383c16fbadd2f9f588bc6bbfa2fb4a32d9205c04
HAdiiiir <hader.jaamal2010@gmail.com>
2023-11-17 06:40:37 -0800

    a

--- null_format.c
+++ null_format.c
@@ -8,9 +8,84 @@
  */
 void check_null(const char *format)
 {
-	if (!format)
-	{
-		write(1, "Error\n", 6);
-		exit(98);
-	}
+if (!format)
+{
+while (*format >= '0' && *format <= '9')
+{
+width = width * 10 + (*format - '0');
+format++;
+}
+
+switch (*format)
+{
+case 'c':
+count += putchar(va_arg(args, int));
+break;
+case 's':
+if (width > 0)
+{
+count += printf("%*s", width, va_arg(args, char *));
+}
+else
+{
+count += printf("%s", va_arg(args, char *));
+}
+break;
+case 'p':
+count += printf("%p", va_arg(args, void *));
+break;
+case 'b':
+print_binary(va_arg(args, unsigned int));
+count += sizeof(unsigned int) * 8;
+break;
+case 'd':
+case 'i':
+if (width > 0)
+{
+count += printf("%+*d", width, va_arg(args, int));
+}
+else
+{
+count += printf("%+d", va_arg(args, int));
+}
+break;
+case 'o':
+if (width > 0)
+{
+count += printf("%#*o", width, va_arg(args, unsigned int));
+}
+else
+{
+count += printf("%#o", va_arg(args, unsigned int));
+}
+break;
+case 'x':
+case 'X':
+if (width > 0)
+{
+count += printf("%#*x", width, va_arg(args, unsigned int));
+}
+else
+{
+count += printf("%#x", va_arg(args, unsigned int));
+}
+break;
+default:
+count += putchar('%');
+count += putchar(*format);
+break;
+}
+}
+else
+{
+count += putchar(*format);
+}
+format++;
+}
+
+va_end(args);
+
+return (count);
+}
+}
 }
