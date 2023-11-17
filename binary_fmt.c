46acd2f837a99d91c21f886612a364ddece21be3
HAdiiiir <hader.jaamal2010@gmail.com>
2023-11-17 04:59:23 -0800

    a

--- binary_fmt.c
+++ binary_fmt.c
@@ -9,31 +9,31 @@
  */
 mk_buffer binary_fmt(mk_buffer buffer, va_list args)
 {
-int j, len, temp;
-unsigned int num;
-int binary_arr[1000];
+	int j, len, temp;
+	unsigned int num;
+	int binary_arr[1000];
 
-num = va_arg(args, unsigned int);
+	num = va_arg(args, unsigned int);
 
-len = 0;
-while (num > 0)
-{
-temp = 0;
-temp = num % 2;
-binary_arr[len] = temp + 48;
-num /= 2;
-len++;
-}
+	len = 0;
+	while (num > 0)
+	{
+		temp = 0;
+		temp = num % 2;
+		binary_arr[len] = temp + 48;
+		num /= 2;
+		len++;
+	}
 
-j = len - 1;
-while (j >= 0)
-{
-*buffer.box = binary_arr[j];
-buffer.box++;
-buffer.size++;
-j--;
-}
+	j = len - 1;
+	while (j >= 0)
+	{
+		*buffer.box = binary_arr[j];
+		buffer.box++;
+		buffer.size++;
+		j--;
+	}
 
-buffer.box--;
-return (buffer);
+	buffer.box--;
+	return (buffer);
 }
